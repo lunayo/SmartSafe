174541b8c3bab75bdc3eafa322b25044378d8a15
Iskandar Goh <incredible2288@gmail.com>
2015-02-07 20:42:35 +0800

    Initial Commit

---
+++ SmartSafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartSafe
+//
+//  Created by Lunayo on 07/02/2015.
+//  Copyright (c) 2015 Lunayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartSafe/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SmartSafe
+//
+//  Created by Lunayo on 07/02/2015.
+//  Copyright (c) 2015 Lunayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
